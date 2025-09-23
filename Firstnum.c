e9a1a0d2246359b7e5885443e69a5f197b8c9da4
Makwan Mahipat <makwanamahipat273@gmail.com>
2025-09-23 20:11:55 +0530

    Create Firstnum.c
    Welcome to my website visit

---
+++ Firstnum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ #include<conio.h> 
+  void main() 
+   { int a,b,c; 
+    clrscr();
+     printf("enter a first number:"); 
+     scanf("%d",&a); 
+     printf("enter a ending number:"); 
+     scanf("%d",&b); 
+      for(c=a; c<=b; c++) 
+      { 
+      printf("%d\n",c);
+      } 
+       printf("welcome"); 
+       getch(); 
+}
