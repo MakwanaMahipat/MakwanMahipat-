a0b9ab86533a5cae3c50f85b83e4c9aea3f5ee68
Makwana Mahipat <makwanamahipat273@gmail.com>
2025-09-27 17:35:51 +0530

    Create Sum.c unit 3

---
+++ Sum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int  sum(int a,int b);
+int main()
+{     
+    int a,b;
+     clrscr();
+     printf("enter first num:");
+     scanf("%d",&a);
+      printf("enter first num:");
+     scanf("%d",&b);
+     
+     if(a,b){
+     printf("sum is: %d\n",a+b);
+     }
+          return 0;
+ }
+ int sum(int a,int b)
+ {
+     printf("%d\n",(a+b)); 
+     }
+    
