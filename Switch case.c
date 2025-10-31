a10282fa47a5e374e2eea169a66e3dd123698cbf
Makwana Mahipat <makwanamahipat273@gmail.com>
2025-10-31 19:42:08 +0530

    Add switch case for user input handling
    Implement a switch case structure to handle user input.

---
+++ Switch case.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{    
+   int a;
+   
+  printf("enter a any number :");
+   scanf("%d",&a);
+    switch(a)
+    {
+    case 1:
+        printf("\nwelcome %d ",a);
+        break;
+    case 2:
+        printf("\nhello%d",a);    
+        break;
+    case 3:
+             printf("\nhi %d",a);
+                break;
+    default:
+    printf("\n invalid num");
+    v}
+    getch();
+}
